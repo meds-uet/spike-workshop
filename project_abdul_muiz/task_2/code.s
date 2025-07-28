9f68b032b27239904be79cea8c92a62e6a370fc5
abdmz162 <abdmz.uetian23@gmail.com>
2025-07-28 16:20:19 +0500

    completed workshop by abdmz162 (2023-EE-162)

---
+++ project_abdul_muiz/task_2/code.s
@@ -0,0 +1,22 @@
+.data
+arr: .word 1,2,3,4,5,6,7,8,9,10
+
+.text
+.globl _start
+
+_start:
+    li   t0, 0 #i
+    li   t1, 10 #n
+    la   t4, arr
+    li   t3, 0 #total
+loop:
+    beq  t0, t1, done    # should stop when t0 == 10?
+    lw   t2, 0(t4)       # wrong base register
+    add  t3, t3, t2      # t3 uninitialized
+    addi t0, t0, 1
+    addi t4,t4,4
+    j loop      # inefficient jump
+done:
+    mv   a0, t3
+    li   a7, 93
+    ecall
